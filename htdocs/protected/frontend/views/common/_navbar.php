3515e3b096051d7833addd53679579619d4ad1db
Eduardo Silva <eduardo@open-ecommerce.org>
2019-03-21 21:39:00 +0000

    top menu

--- htdocs/protected/frontend/views/common/_navbar.php
+++ htdocs/protected/frontend/views/common/_navbar.php
@@ -4,7 +4,7 @@ use frontend\widgets\GlobalMenu;
 ?>
 <div class="container">
   <nav id="menu" class="navbar">
-      <div class="navbar-header"><span id="category" class="visible-xs"><?php echo UsniAdaptor::t('productCategories', 'Categories'); ?></span>
+      <div class="navbar-header"><span id="category" class="visible-xs"><?php echo UsniAdaptor::t('productCategories', 'Workshops for ->'); ?></span>
       <button type="button" class="btn btn-navbar navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse"><i class="fa fa-bars"></i></button>
     </div>
     <div class="collapse navbar-collapse navbar-ex1-collapse">
