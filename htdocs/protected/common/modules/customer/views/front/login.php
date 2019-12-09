a1c3a3495fd0f5cabebc311d17989c742be30dee
eduardo <eduardo.silva@wirelesslogic.com>
2019-12-09 19:31:41 +0000

    mobile

--- htdocs/protected/common/modules/customer/views/front/login.php
+++ htdocs/protected/common/modules/customer/views/front/login.php
@@ -17,7 +17,7 @@ $this->bodyClass = 'registration login';
             <div class="about-registration">
                 <p>To keep the milongas Balanced, Couple Registration has priority.</p>
                 <p>You are welcome to register as single and you will be in a waiting list until another complementary dancer register.</p>
-                <p>If you have any problem please send us a facebook message <a href="https://www.facebook.com/uktangofestival/">https://www.facebook.com/uktangofestival/</a> and we will contact you within a day.</p>
+                <p>If you have any problem please send us a facebook message <a href="https://www.facebook.com/uktangofestival/">facebook</a> and we will contact you within a day.</p>
                 <p>Alternatively you can email us to: <a href = "mailto: info@uktangofestival.com">info@uktangofestival.com</a></p>
             </div>
             <div class="row">
