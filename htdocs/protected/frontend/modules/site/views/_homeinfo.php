8de38c97d33ce355f7126fb5b77265b0fc992e62
eduardo <eduardo.silva@wirelesslogic.com>
2019-12-15 02:28:24 +0000

    menu with select case

--- htdocs/protected/frontend/modules/site/views/_homeinfo.php
+++ htdocs/protected/frontend/modules/site/views/_homeinfo.php
@@ -1,18 +1,26 @@
 <?php
 use usni\UsniAdaptor;
+use usni\library\utils\CustomerTypeUtil;
 use yii\helpers\Html;
 use yii\helpers\Url;
 
 if ( Yii::$app->user->isGuest )
     return Yii::$app->getResponse()->redirect('/customer/site/login',302);
 ?>
+
+<?php
+$model  = UsniAdaptor::app()->user->getIdentity();
+?>
 <h3 class="home">UK Tango Festival & Championship 2020</h3>
 <div class="row">
     <div class="col-xs-12">
         <div class="home-info">
+            <p>Dear <?= $model->name; ?>,</p>
             <p>In this site you can buy Festival, Competitor Passes and Workshops.</p>
             <p>You can also see the detail of the items you bought in <?= Html::a('Your Account', ['/customer/site/my-account']) ?>.</p>
             <p>If you have any problem or inquire you can email us to: <a href = "mailto: info@uktangofestival.com">info@uktangofestival.com</a></p>
+            <br>
+            <br>
             <br>
             <br>
         </div>
--- htdocs/protected/frontend/views/common/_navbar.php
+++ htdocs/protected/frontend/views/common/_navbar.php
@@ -1,7 +1,66 @@
 <?php
-
+use usni\UsniAdaptor;
+use usni\library\utils\CustomerTypeUtil;
 use yii\bootstrap\NavBar;
 use yii\bootstrap\Nav;
+
+$model  = UsniAdaptor::app()->user->getIdentity();
+
+switch ($model->type) {
+    case CustomerTypeUtil::CUSTOMER_TYPE_FESTIVAL_SINGLE:
+        $items = [
+            ['label' => 'Festival Passes (single)', 'url' => ['/site/about']],
+            [
+                'label' => 'Workshops',
+                'items' => [
+                    ['label' => 'Leaders', 'url' => '#'],
+                    ['label' => 'Followers', 'url' => '#'],
+                    ['label' => 'Couples', 'url' => '#'],
+                ],
+            ],
+        ];
+        break;
+    case CustomerTypeUtil::CUSTOMER_TYPE_FESTIVAL_COUPLE:
+        $items = [
+            ['label' => 'Festival Passes (couples)', 'url' => ['/site/about']],
+            [
+                'label' => 'Workshops',
+                'items' => [
+                    ['label' => 'Leaders', 'url' => '#'],
+                    ['label' => 'Followers', 'url' => '#'],
+                    ['label' => 'Couples', 'url' => '#'],
+                    ['label' => 'Competitors', 'url' => '#'],
+                ],
+            ],
+        ];
+        break;
+    case CustomerTypeUtil::CUSTOMER_TYPE_COMPETITOR:
+        $items = [
+            ['label' => 'Competitor Passes', 'url' => ['/site/about']],
+            [
+                'label' => 'Workshops',
+                'items' => [
+                    ['label' => 'Leaders', 'url' => '#'],
+                    ['label' => 'Followers', 'url' => '#'],
+                    ['label' => 'Couples', 'url' => '#'],
+                    ['label' => 'Competitors', 'url' => '#'],
+                ],
+            ],
+        ];
+        break;
+    default: //volunteer, concesion, workshop
+        $items = [
+            [
+                'label' => 'Workshops',
+                'items' => [
+                    ['label' => 'Leaders', 'url' => '#'],
+                    ['label' => 'Followers', 'url' => '#'],
+                    ['label' => 'Couples', 'url' => '#'],
+                ],
+            ],
+        ];
+}
+
 ?>
 <div class="container">
 <?php
@@ -16,19 +75,7 @@ NavBar::begin([
 ]);
 echo Nav::widget([
     'options' => ['class' => 'navbar-nav navbar-left'],
-    'items' => [
-        ['label' => 'Festival Passes', 'url' => ['/site/about']],
-        ['label' => 'Competitor Passes', 'url' => ['/site/about']],
-        [
-            'label' => 'Workshops',
-            'items' => [
-                ['label' => 'Leaders', 'url' => '#'],
-                ['label' => 'Followers', 'url' => '#'],
-                ['label' => 'Couples', 'url' => '#'],
-                ['label' => 'Competitors', 'url' => '#'],
-            ],
-        ],
-    ],
+    'items' => $items,
 ]);
 NavBar::end();
 ?>
