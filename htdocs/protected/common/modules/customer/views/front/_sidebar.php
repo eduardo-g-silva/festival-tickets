56cb3aea19f23448f1cad6d1c4e743d0cd379b31
eduardo <eduardo.silva@wirelesslogic.com>
2019-12-15 03:05:57 +0000

    restrict my account

--- htdocs/protected/common/modules/customer/views/front/_sidebar.php
+++ htdocs/protected/common/modules/customer/views/front/_sidebar.php
@@ -11,12 +11,12 @@ use usni\UsniAdaptor;
 <div class="col-sm-3 hidden-xs" id="column-left">
 <nav>
   <ul id="accountsidebarlist">
-      <li><?php echo Html::a(UsniAdaptor::t('users', 'My Account'), UsniAdaptor::createUrl('/customer/site/my-account')); ?></li>
-      <li><?php echo Html::a(UsniAdaptor::t('users', 'Edit Profile'), UsniAdaptor::createUrl('/customer/site/edit-profile')); ?></li>
-      <li><?php echo Html::a(UsniAdaptor::t('users', 'Change Password'), UsniAdaptor::createUrl('/customer/site/change-password')) ?></li>    
       <li><?php echo Html::a(UsniAdaptor::t('order', 'My Orders'), UsniAdaptor::createUrl('/customer/site/order-history')) ?></li>
-      <li><?php echo Html::a(UsniAdaptor::t('wishlist', 'Wishlist'), UsniAdaptor::createUrl('wishlist/default/view')) ?></li>
-      <li><?php echo Html::a(UsniAdaptor::t('customer', 'My Downloads'), UsniAdaptor::createUrl('customer/site/downloads')) ?></li>
+      <li><?php echo Html::a(UsniAdaptor::t('users', 'My Account'), UsniAdaptor::createUrl('/customer/site/my-account')); ?></li>
+      <li><?php //echo Html::a(UsniAdaptor::t('users', 'Edit Profile'), UsniAdaptor::createUrl('/customer/site/edit-profile')); ?></li>
+      <li><?php echo Html::a(UsniAdaptor::t('users', 'Change Password'), UsniAdaptor::createUrl('/customer/site/change-password')) ?></li>    
+      <li><?php //echo Html::a(UsniAdaptor::t('wishlist', 'Wishlist'), UsniAdaptor::createUrl('wishlist/default/view')) ?></li>
+      <li><?php //echo Html::a(UsniAdaptor::t('customer', 'My Downloads'), UsniAdaptor::createUrl('customer/site/downloads')) ?></li>
   </ul>
 </nav>
 </div>
