2f7fcbecfde4f9f1d4e158445e2d34ec84f2d8e6
eduardo <eduardo.silva@wirelesslogic.com>
2019-12-15 02:45:21 +0000

    menu categories

--- htdocs/protected/frontend/views/common/_navbar.php
+++ htdocs/protected/frontend/views/common/_navbar.php
@@ -9,13 +9,13 @@ $model  = UsniAdaptor::app()->user->getIdentity();
 switch ($model->type) {
     case CustomerTypeUtil::CUSTOMER_TYPE_FESTIVAL_SINGLE:
         $items = [
-            ['label' => 'Festival Passes (single)', 'url' => ['/site/about']],
+            ['label' => 'Festival Passes (single)', 'url' => ['/catalog/productCategories/site/view?id=5']],
             [
                 'label' => 'Workshops',
                 'items' => [
-                    ['label' => 'Leaders', 'url' => '#'],
-                    ['label' => 'Followers', 'url' => '#'],
-                    ['label' => 'Couples', 'url' => '#'],
+                    ['label' => 'Leaders', 'url' => '/catalog/productCategories/site/view?id=4'],
+                    ['label' => 'Followers', 'url' => '/catalog/productCategories/site/view?id=4'],
+                    ['label' => 'Couples', 'url' => '/catalog/productCategories/site/view?id=4'],
                 ],
             ],
         ];
@@ -26,24 +26,23 @@ switch ($model->type) {
             [
                 'label' => 'Workshops',
                 'items' => [
-                    ['label' => 'Leaders', 'url' => '#'],
-                    ['label' => 'Followers', 'url' => '#'],
-                    ['label' => 'Couples', 'url' => '#'],
-                    ['label' => 'Competitors', 'url' => '#'],
+                    ['label' => 'Leaders', 'url' => '/catalog/productCategories/site/view?id=4'],
+                    ['label' => 'Followers', 'url' => '/catalog/productCategories/site/view?id=4'],
+                    ['label' => 'Couples', 'url' => '/catalog/productCategories/site/view?id=4'],
                 ],
             ],
         ];
         break;
     case CustomerTypeUtil::CUSTOMER_TYPE_COMPETITOR:
         $items = [
-            ['label' => 'Competitor Passes', 'url' => ['/site/about']],
+            ['label' => 'Competitor Passes', 'url' => ['/catalog/productCategories/site/view?id=6']],
             [
                 'label' => 'Workshops',
                 'items' => [
-                    ['label' => 'Leaders', 'url' => '#'],
-                    ['label' => 'Followers', 'url' => '#'],
-                    ['label' => 'Couples', 'url' => '#'],
-                    ['label' => 'Competitors', 'url' => '#'],
+                    ['label' => 'Leaders', 'url' => '/catalog/productCategories/site/view?id=4'],
+                    ['label' => 'Followers', 'url' => '/catalog/productCategories/site/view?id=4'],
+                    ['label' => 'Couples', 'url' => '/catalog/productCategories/site/view?id=4'],
+                    ['label' => 'Competitors', 'url' => '/catalog/productCategories/site/view?id=4'],
                 ],
             ],
         ];
@@ -53,9 +52,9 @@ switch ($model->type) {
             [
                 'label' => 'Workshops',
                 'items' => [
-                    ['label' => 'Leaders', 'url' => '#'],
-                    ['label' => 'Followers', 'url' => '#'],
-                    ['label' => 'Couples', 'url' => '#'],
+                    ['label' => 'Leaders', 'url' => '/catalog/productCategories/site/view?id=4'],
+                    ['label' => 'Followers', 'url' => '/catalog/productCategories/site/view?id=4'],
+                    ['label' => 'Couples', 'url' => '/catalog/productCategories/site/view?id=4'],
                 ],
             ],
         ];
