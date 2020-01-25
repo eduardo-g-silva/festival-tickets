ebf68eaaa506c3f0dcfcf22a332489b68222ca93
eduardo <eduardo.silva@wirelesslogic.com>
2020-01-25 19:35:26 +0000

    new notification

--- htdocs/protected/frontend/modules/site/views/_homeinfo.php
+++ htdocs/protected/frontend/modules/site/views/_homeinfo.php
@@ -17,10 +17,12 @@ $model  = UsniAdaptor::app()->user->getIdentity();
         <div class="home-info">
             <p>Dear <?= $model->name; ?>,</p>
             <p>Your email has been validated!</p>
-            <p>If you have registered as single, you will be in a waiting list until another complementary dancer registers. We will send you an email when this takes place.</p>
-            <p>If you have registered as a couple shortly you will receive a confirmation email for you to login and buy your tickets.</p>
-            <p>If you have any problem or inquire you can email us to: <a href = "mailto: info@uktangofestival.com">info@uktangofestival.com</a></p>
+            <p>If you have registered as single, you will be on a waiting list until another complementary dancer registers. We will send you an email when this takes place.</p>
+            <p>If you have registered as a couple (Social Dancers or Competitors) shortly you will receive a confirmation email for you to login and buy your tickets.</p>
+            <p>If you have any problem or an enquiry you can email us at: <a href = "mailto: info@uktangofestival.com">info@uktangofestival.com</a></p>
             <br>
+            <p>You can also log in again in your account to look for updates at any time.</p>
+            <p>Couple registrations should see updates on their accounts in no longer than 48 hours!</p>
             <br>
             <br>
             <br>
